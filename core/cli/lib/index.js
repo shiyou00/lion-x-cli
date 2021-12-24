80a375af889b94de6a5615e66328f1c10beba66b
frankshi <frankshi@easyops.cn>
2021-12-24 08:12:03 +0800

    add log module and add checknodeversion

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,13 +1,30 @@
 'use strict';
-// require 支持加载.js .json .node
+const colors = require("colors/safe"); // 控制台输出有颜色
+const semver = require("semver"); // semver 版本控制比较
+const { LOWEST_NODE_VERSION } = require("./consts");
+
 const pkg = require("../package.json");
+const log = require("@lion-x/log")
 
 function core() {
-    console.log("checkPkgVersion",checkPkgVersion())
+    try{
+        checkPkgVersion();
+        checkNodeVersion();
+    }catch (e){
+        log.error(e.message);
+    }
 }
 
 function checkPkgVersion(){
-    return pkg.version;
+    log.info("cli",pkg.version);
+}
+
+function checkNodeVersion(){
+    const currentVersion = process.version;
+    const lowestVersion = LOWEST_NODE_VERSION;
+    if(!semver.gte(currentVersion,lowestVersion)){
+        throw new Error(colors.red(`lion-x 需要安装v${lowestVersion}以上版本的Node.js`))
+    }
 }
 
 module.exports = core;
