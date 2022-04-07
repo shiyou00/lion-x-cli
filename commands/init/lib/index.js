dfa8c3741c17957336513dfdbb4aa0369d045477
frankshi <8533334@qq.com>
2022-04-07 15:20:27 +0800

    判断执行命令文件夹是否为空

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -1,6 +1,7 @@
 'use strict';
 const log = require("@lion-x/log");
-const Command = require("@lion-x/command")
+const Command = require("@lion-x/command");
+const fs = require("fs");
 
 class initCommand extends Command{
     init(){
@@ -10,7 +11,26 @@ class initCommand extends Command{
         log.verbose('force', this.force);
     }
     exec() {
-
+        try {
+            this.prepare();
+        }catch (e) {
+            log.error(e.message);
+        }
+    }
+    prepare(){
+        const localPath = process.cwd();
+        if(!this.isDirEmpty(localPath)){
+            // 文件不为空时进入，判断是否强制更新逻辑
+            console.log(localPath,"不为空");
+        }
+    }
+    isDirEmpty(localPath){
+        let fileList = fs.readdirSync(localPath);
+        // 文件过滤的逻辑
+        fileList = fileList.filter(file => (
+            !file.startsWith('.') && ['node_modules'].indexOf(file) < 0
+        ));
+        return !fileList || fileList.length <= 0;
     }
 }
 function init(argv){
